7c83ae817cc0e53f8886ecb4c319a9efc12d935c
Cailo.k.e.y <unsocialcarlos@gmail.com>
2024-10-08 16:20:58 +0200

    3.1

--- task/App.js
+++ task/App.js
@@ -1,4 +1,14 @@
-// Función que se ejecuta cuando el botón es clicado
-function saludar() {
-    alert("¡Hola! Has hecho clic en el botón.");
-}
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Sirve el archivo HTML
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// Puerto donde se ejecutará el servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
