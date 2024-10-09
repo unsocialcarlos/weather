8539f55e378817f46b9a53f1aac8d973ecaf106c
Cailo.k.e.y <unsocialcarlos@gmail.com>
2024-10-09 18:58:48 +0200

    3.5

---
+++ task/components/consumirApi.js
@@ -0,0 +1,20 @@
+const aplicacion = document.querySelector('.container');
+
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+fetch(url)
+    .then(res => res.json()).then(data => {
+        data.forEach(usuario => {
+
+            const p = document.createElement('p')
+            p.setAttribute('id', usuario.id)
+            p.innerHTML = usuario.name
+            p.addEventListener('click', function(){
+                window.location.href =`./usuario.html?id=${usuario.id}`
+        })
+            
+            aplicacion.appendChild(p)
+        });
+
+    })
+    .catch(err => console.log(err));
---
+++ task/components/usuario.js
@@ -0,0 +1,22 @@
+const aplicacion = document.querySelector('.container')
+const getUrl= new URLSearchParams(window.location.search)
+id = getUrl.get('id')
+const url = 'https://jsonplaceholder.typicode.com/users'
+
+console.log(`${url}/${id}`)
+
+fetch(`${url}/${id}`)
+.then(res => res.json())
+.then(data=>{
+    const name = document.createElement('p')
+    name.innerHTML= data.name
+    const email = document.createElement('p')
+    email.innerHTML= data.email
+    aplicacion.appendChild(name)
+    aplicacion.appendChild(email)
+
+})
+.catch (err => console.log(err))
+
+
+
